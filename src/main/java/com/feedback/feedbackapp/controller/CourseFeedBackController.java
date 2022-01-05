e67032f4c9b3e68af115d18bf175178521d8e6e4
Ruchi Jain <ruchismailbox@gmail.com>
2022-01-04 20:25:02 -0600

    updated CourseFeedBack Controller

--- src/main/java/com/feedback/feedbackapp/controller/CourseFeedBackController.java
+++ src/main/java/com/feedback/feedbackapp/controller/CourseFeedBackController.java
@@ -1,9 +1,65 @@
 package com.feedback.feedbackapp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.feedback.feedbackapp.model.Course;
+import com.feedback.feedbackapp.model.CourseFeedBack;
+import com.feedback.feedbackapp.service.CourseFeedBackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping(path="/api")
 public class CourseFeedBackController {
+
+    private CourseFeedBackService courseFeedBackService;
+    private static final Logger LOGGER = Logger.getLogger(CourseFeedBackController.class.getName());
+
+    @Autowired
+    public void setCourseFeedBackService(CourseFeedBackService courseFeedBackService){
+        this.courseFeedBackService = courseFeedBackService;
+    }
+
+    //to get all the courses
+    // http://localhost:9092/api/coursefeedbacks
+    @GetMapping(path = "/coursefeedbacks")
+    public List<Course> getCourseFeedBacks() {
+        LOGGER.info("calling getCourseFeedBacks method from controller");
+        return courseFeedBackService.getCourseFeedBacks();
+    }
+
+    //to get a single course feedback
+    // http://localhost:9092/api/coursefeedbacks/1
+    @GetMapping(path = "/coursefeedbacks/{courseFeedBackId}")
+    public Optional getCourseFeedBack(@PathVariable Long courseFeedBackId){
+        LOGGER.info("calling getCourseFeedBack method from controller");
+        return courseFeedBackService.getCourseFeedBack(courseFeedBackId);
+    }
+
+    //to create a course
+    // http://localhost:9092/api/coursefeedbacks
+    @PostMapping(path = "/coursefeedbacks")
+    public Course createCourseFeedBack(@RequestBody CourseFeedBack courseFeedBackObject){
+        LOGGER.info("calling createCourseFeedBack method from controller");
+        return courseFeedBackService.createCourse(courseFeedBackObject);
+    }
+
+    //to update a course
+    // http://localhost:9092/api/courses/1
+    @PutMapping(path = "/coursefeedbacks/{courseFeedBackId}")
+    public Course updateCourseFeedBack(@PathVariable(value = "courseFeedBackId") Long courseFeedBackId,
+                               @RequestBody CourseFeedBack courseFeedBackObject){
+        LOGGER.info("calling updateCourseFeedBack method from controller");
+        return courseFeedBackService.updateCourseFeedBack(courseFeedBackId,courseFeedBackObject);
+    }
+
+    //to delete a course
+    // http://localhost:9092/api/courses/1
+    @DeleteMapping(path="/coursefeedbacks/{courseFeedBackId}")
+    public Optional<Course> deleteCourseFeedBack(@PathVariable(value = "courseFeedBackId") Long courseFeedBackId){
+        LOGGER.info("calling deleteCourseFeedBack method from controller");
+        return courseFeedBackService.deleteCourseFeedBack(courseFeedBackId);
+    }
 }
--- src/main/java/com/feedback/feedbackapp/service/CourseFeedBackService.java
+++ src/main/java/com/feedback/feedbackapp/service/CourseFeedBackService.java
@@ -1,4 +1,24 @@
 package com.feedback.feedbackapp.service;
 
+import com.feedback.feedbackapp.model.Course;
+import com.feedback.feedbackapp.model.CourseFeedBack;
+
+import java.util.List;
+import java.util.Optional;
+
 public class CourseFeedBackService {
+    public List<Course> getCourseFeedBacks() {
+    }
+
+    public Optional getCourseFeedBack(Long courseFeedBackId) {
+    }
+
+    public Course createCourse(CourseFeedBack courseFeedBackObject) {
+    }
+
+    public Course updateCourseFeedBack(Long courseFeedBackId, CourseFeedBack courseFeedBackObject) {
+    }
+
+    public Optional<Course> deleteCourseFeedBack(Long courseFeedBackId) {
+    }
 }
