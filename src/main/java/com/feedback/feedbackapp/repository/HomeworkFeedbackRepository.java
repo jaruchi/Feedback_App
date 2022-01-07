3ee79fe7e93e26698f13d69e0f7a441b7971e537
Ruchi Agarwal <ruchismailbox@gmail.com>
2022-01-07 14:09:59 -0600

    added homeworkfeedback repository

---
+++ src/main/java/com/feedback/feedbackapp/repository/HomeworkFeedbackRepository.java
@@ -0,0 +1,17 @@
+package com.feedback.feedbackapp.repository;
+
+import com.feedback.feedbackapp.model.HomeworkFeedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface HomeworkFeedbackRepository extends JpaRepository<HomeworkFeedback,Long> {
+    List<HomeworkFeedback> findByHomeworkId(Long homeworkId);
+
+    List<HomeworkFeedback> findByUserId(Long userId);
+
+    Optional<HomeworkFeedback> findByUserIdAndHomeworkId(Long userId, Long homeworkId);
+}
